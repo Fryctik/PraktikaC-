49564bdfcf7ce5dc070dad19e1001537efbaf85c
Fryctik <83564951+Fryctik@users.noreply.github.com>
2023-03-04 13:57:12 +0700

    Add files via upload

---
+++ 2.1/d/d/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace d
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+//String j = "ad"; 
+            //String s = "tyasfdyawtsdfaysvdatfdatwsduasasdhaidsdfdif"; 
+ 
+            //int temp = 0; 
+            //for(int i = 0; i < j.Length; i++) 
+            //{ 
+            //    for (int k = 0; k < s.Length; k++) 
+            //    { 
+            //        if (j[i] == s[k]) 
+            //        { 
+            //            temp++; 
+            //        } 
+            //    } 
+                 
+            //} 
+            //Console.WriteLine(temp); 
+ 
+            //Console.WriteLine(temp); 
+ 
+            //int[] candidates = { 1, 2, 3, 4, 5, 6, 7, 8, 9}; 
+            //List<int> array = new List<int>(); 
+            //int target = 10; 
+            //int temp = 0; 
+            //for(int i =0; i < candidates.Length; i++) 
+            //{ 
+            //    temp = candidates[i]; 
+            //    array.Add(temp); 
+            //    for(int j = 0; j < candidates.Length; j++) 
+            //    { 
+            //        if(temp != target) 
+            //        { 
+            //            temp += candidates[j]; 
+            //            array.Add(temp); 
+            //        } 
+            //    } 
+            //} 
+ 
+            //for(int i = 0; i < array.Length; i++) 
+            //{ 
+            //    Console.WriteLine(array[i]); 
+            //} 
+ 
+            //int[] nums = new int[20]; 
+            //Random rng = new Random(); 
+            //bool temp = false; 
+            //for(int i = 0; i < nums.Length; i++) 
+            //{ 
+            //    nums[i] = rng.Next(10); 
+            //} 
+ 
+            //for(int i = 0; i < nums.Length; i++) 
+            //{ 
+            //    for(int j = 1; j < nums.Length; j++) 
+            //    { 
+            //        if (nums[i] == nums[j]) 
+            //        { 
+            //            temp= true; 
+            //        } 
+            //    } 
+            //} 
+ 
+            //Console.WriteLine(temp);
+        }
+    }
+}
